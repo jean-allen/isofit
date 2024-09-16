d79ebf51b558eabd13a9ae6e6ace7739b57c4e06
jean-allen <jean.allen@geog.ucsb.edu>
2024-09-16 12:52:49 -0700

    added sim CWC example

---
+++ examples/simultaneous_CWC/data/lut_full/LUT_AOT550-0.4006_H2OSTR-0.2500.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+/Users/jeaallen/Documents/simultaneous_EWT/6sV2.1/sixsV2.1 < /Users/jeaallen/Documents/simultaneous_EWT/isofit/examples/simultaneous_CWC/data/lut_full/LUT_AOT550-0.4006_H2OSTR-0.2500.inp > /Users/jeaallen/Documents/simultaneous_EWT/isofit/examples/simultaneous_CWC/data/lut_full/AOT550-0.4006_H2OSTR-0.2500
+cd $cwd
